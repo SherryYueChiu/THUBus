d22394ed942359d95c11a55eee7d49e72eaf8da3
Sherry <sherry@lulainno.com>
2022-03-22 22:38:16 +0800

    更新樣式

--- readPage.php
+++ readPage.php
@@ -1,7 +1,7 @@
  <?php
  header("Access-Control-Allow-Origin: *");
- header("Access-Control-Allow-Methods:*");
- header("Access-Control-Allow-Headers:x-requested-with,content-type");
+ header("Access-Control-Allow-Methods: *");
+ header("Access-Control-Allow-Headers: x-requested-with,Origin, Methods, Content-Type, Authorization");
  $URL = $_GET["URL"];
  echo file_get_contents($URL);
 ?>
