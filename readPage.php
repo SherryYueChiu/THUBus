e38a0588c6388e6c165693aa97be21742e05770b
Sherry <sherry@lulainno.com>
2022-04-08 20:21:40 +0800

    Cors測試

--- readPage.php
+++ readPage.php
@@ -2,6 +2,8 @@
  header("Access-Control-Allow-Origin: *");
  header("Access-Control-Allow-Methods: *");
  header("Access-Control-Allow-Headers: x-requested-with,Origin, Methods, Content-Type, Authorization");
+ header('content-type: text/html; charset=utf-8');
+ header("Referrer-Policy: unsafe-url");
  $URL = $_GET["URL"];
  echo file_get_contents($URL);
 ?>
